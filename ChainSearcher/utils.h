da3295c4918f89ba58ae66bbf4beb133cbb5cbee
1 <1@gmail.com>
2017-07-12 14:37:44 +0300

    Add files to version control. Add gitignore file to hide them.

---
+++ ChainSearcher/utils.h
@@ -0,0 +1,8 @@
+#ifndef __Utils__
+#define __Utils__
+
+
+typedef unsigned __int64   uint64_t; //!< Беззнаковое целое число длиной 8 байт
+const int NUMBER_SIZE = 6; //!< Размер чисел, которые мы будем читать, анализировать (6 байтов)
+
+#endif
